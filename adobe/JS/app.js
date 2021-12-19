2b8a6c0a9ab3bd0d2fce12bf42adc3381707ecdc
박유진(Park Yoojin) <93498523+yoojin-park19@users.noreply.github.com>
2021-12-19 19:18:50 +0900

    add navigation menu button rotate

--- adobe/JS/app.js
+++ adobe/JS/app.js
@@ -3,21 +3,44 @@
 
 const btns = document.querySelectorAll(".btn");
 const containers = document.querySelectorAll(".container");
+const lists =  document.querySelectorAll(".list");
 
 btns.forEach((btn, index) => {
     btn.addEventListener("click", function menuBar() {
-    const hasOn = containers[index].classList.contains("on")
+    const hasOn = containers[index].classList.contains("on");
     if (!hasOn ) {
       containers.forEach ((container) => {
       container.classList.remove("on");
       });
       containers[index].classList.toggle("on");
-    }
+      }
     else {
       containers.forEach ((container) => {
       container.classList.remove("on");
-    })};
-})})
+      })
+    }
+  })
+})
+
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", function angle() {
+  const hasPop = lists[index].classList.contains("pop");
+  if (!hasPop ) {
+    lists.forEach ((list) => {
+    list.classList.remove("pop");
+    });
+    lists[index].classList.toggle("pop");
+    }
+  else {
+    lists.forEach ((list) => {
+    list.classList.remove("pop");
+    })
+  }
+})
+})
+
+
+
 
 
 //  모달
